a3b978f6e01ad41c103ffc59fbcec8643b0788c7
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:29:53 -0600

    Fixed indentation

--- Proyecto/4550/ADC SetPoint.c
+++ Proyecto/4550/ADC SetPoint.c
@@ -16,32 +16,26 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF                //Espacio reservado para bootloader
-#define LED_ON       LATA4_bit = 1   //Asigna el estado de indicador visual
-#define LED_OFF      LATA4_bit = 0   //Asigna el estado de indicador visual
-                                     //Tome en cuenta que en versiones anteriores se hacia uso del pin A1
+#pragma orgall 0x1FFF           // Espacio reservado para bootloader
+#define LED_ON LATA4_bit = 1    // Asigna el estado de indicador visual
+#define LED_OFF LATA4_bit = 0   // Asigna el estado de indicador visual
+                                // Tome en cuenta que en versiones anteriores se hacia uso del pin A1
 
 unsigned int ValorPot;
 
-void main() org 0x2000               //vector de inicio usando bootloader MICROSIDE
+void main() org 0x2000          // vector de inicio usando bootloader MICROSIDE
 {
-    LATA4_bit = 0;                    //SALIDA A4
-    TRISA4_bit = 0;                   //PIN COMO SALIDA
-    TRISA0_bit = 1;                   //PIN como entrada
+   LATA4_bit = 0;               // SALIDA A4
+   TRISA4_bit = 0;              // PIN COMO SALIDA
+   TRISA0_bit = 1;              // PIN como entrada
 
-    while (1)
-    {
-        ValorPot=ADC_Read(0);         //Leer el valor del potenciometro
+   while ( 1 ) {
+      ValorPot = ADC_Read( 0 ); // Leer el valor del potenciometro
 
-        if(ValorPot <= 512)           //Compara el valor de el ADC con 512
-        {
-            LED_OFF;                  //Apaga el LED
-        }
-
-        else 
-        {
-          LED_ON;                     //Enciende el LED
-        }
-
-    }
+      if ( ValorPot <= 512 ) {  // Compara el valor de el ADC con 512
+         LED_OFF;               // Apaga el LED
+      } else {
+         LED_ON;                // Enciende el LED
+      }
+   }
 }
--- Proyecto/45K50/ADC SetPoint.c
+++ Proyecto/45K50/ADC SetPoint.c
@@ -16,35 +16,27 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-
-#pragma orgall 0x1FFF                    //Espacio reservado para bootloader
-#define LED_ON       LATA4_bit = 1       //Asigna el estado de indicador visual en alto
-#define LED_OFF      LATA4_bit = 0       //Asigna el estado de indicador visual en bajo
-                                         //Tome en cuenta que en versiones anteriores se hacia uso del pin A1
-
+#pragma orgall 0x1FFF           // Espacio reservado para bootloader
+#define LED_ON LATA4_bit = 1    // Asigna el estado de indicador visual en alto
+#define LED_OFF LATA4_bit = 0   // Asigna el estado de indicador visual en bajo
+                                // Tome en cuenta que en versiones anteriores se hacia uso del pin A1
 
 unsigned int ValorPot;
 
-void main() org 0x2000                   //vector de inicio usando bootloader MICROSIDE
+void main() org 0x2000          // vector de inicio usando bootloader MICROSIDE
 {
-    LATA4_bit = 0;                       //SALIDA A4
-    TRISA4_bit = 0;                      //PIN COMO SALIDA
-    TRISA0_bit = 1;                      //PIN como entrada
-    ANSA0_bit = 1;                       //PIN como analógico
+   LATA4_bit = 0;               // SALIDA A4
+   TRISA4_bit = 0;              // PIN COMO SALIDA
+   TRISA0_bit = 1;              // PIN como entrada
+   ANSA0_bit = 1;               // PIN como analógico
 
-    while (1)
-    {
-        ValorPot=ADC_Read(0);            //Leer el valor del potenciometro
+   while ( 1 ) {
+      ValorPot = ADC_Read( 0 ); // Leer el valor del potenciometro
 
-        if(ValorPot <= 512)              //Compara el valor de el ADC con 512
-        {
-            LED_OFF;                     //Apaga el LED
-        }
-
-        else 
-        {
-          LED_ON;                        //Enciende el LED
-        }
-
-    }
+      if ( ValorPot <= 512 ) {  // Compara el valor de el ADC con 512
+         LED_OFF;               // Apaga el LED
+      } else {
+         LED_ON;                // Enciende el LED
+      }
+   }
 }
